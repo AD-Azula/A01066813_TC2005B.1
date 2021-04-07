1b99d784e2641814f0a83e077a42007c0d3e72ad
AD-Azula <therealdeanandre@gmail.com>
2021-04-07 03:44:26 -0500

    body-parser

--- Lab11/app.js
+++ Lab11/app.js
@@ -7,11 +7,25 @@ console.log("Dios bendiga al npm");
 
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 //Middleware
+app.use(bodyParser.urlencoded({extended: false}));
 app.use((request, response, next) => {
     console.log('Middleware!');
     next(); //Le permite a la petición avanzar hacia el siguiente middleware
+});
+
+app.use('/ruta/nueva-ruta', (request, response, next) => {
+    response.send('<h1>Recien hecha</h1>'); 
+});
+
+app.use('/ruta', (request, response, next) => {
+    response.send('<h1>Bienvenido seas</h1>'); 
+});
+
+app.use('/', (request, response, next) => {
+    response.send('<h1>Disque la ruta raiz</h1>'); 
 });
 
 app.use((request, response, next) => {
